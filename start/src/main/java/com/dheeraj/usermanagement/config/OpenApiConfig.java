fef910ac4a199a3b4590c377e3efd3671186b7c7
dhiraj181818 <dhirajbhambhu18@gmail.com>
2026-06-06 22:48:16 +0530

    Fix Swagger HTTPS URL

--- start/src/main/java/com/dheeraj/usermanagement/config/OpenApiConfig.java
+++ start/src/main/java/com/dheeraj/usermanagement/config/OpenApiConfig.java
@@ -1,11 +1,14 @@
 package com.dheeraj.usermanagement.config;
-
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import java.util.List;
 
 @Configuration
 public class OpenApiConfig {
@@ -19,6 +22,11 @@ public class OpenApiConfig {
                 .bearerFormat("JWT");
 
         return new OpenAPI()
+                .servers(List.of(
+                        new Server()
+                                .url("https://springboot-user-management-production-0066.up.railway.app")
+                                .description("Production Server")
+                ))
                 .info(new Info()
                         .title("User Management API")
                         .version("1.0"))
